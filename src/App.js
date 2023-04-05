dd53ac1321b90f56486b30ae448a96fa52f374b1
revinianz13 <kitesurf@windowslive.com>
2023-04-05 13:17:00 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState} from 'react';
+import useHttp from './hooks/use-http';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+	const [tasks, setTasks] = useState([]);
+
+	const transforTasks = (tasksObj) => {
+		const loadedTasks = [];
+
+		for (const taskKey in tasksObj) {
+			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+		}
+
+		setTasks(loadedTasks);
+	};
+
+	const {
+		isLoading,
+		error,
+		sendRequest: fetchTasks,
+	} = useHttp(
+		transforTasks
+	);
+
+	useEffect(() => {
+		fetchTasks({ url: 'https://revinian-default-rtdb.firebaseio.com/tasks.json' },transforTasks);
+	}, [fetchTasks]);
+
+	const taskAddHandler = (task) => {
+		setTasks((prevTasks) => prevTasks.concat(task));
+	};
+
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	);
+}
+
+export default App;
